199c309ac9bf8fd28e3042f994baf08abd9c45ef
Demilade111 <demiladealuko111@gmail.com>
2024-07-08 06:16:22 +0100

    chore: Update database configuration to use environment variable for database URL and enable SSL connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,23 @@
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 require("dotenv").config();
 
 const DATABASE_URL = process.env.POSTGRESSURL;
 
 const sequelize = new Sequelize(DATABASE_URL, {
-  dialect: "postgres",
+  dialect: pg,
   logging: false,
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
   retry: {
     max: 5,
@@ -30,9 +37,12 @@ const sequelize = new Sequelize(DATABASE_URL, {
 // Test the connection
 sequelize
   .authenticate()
-  .then(() =>
-    console.log("Database connection has been established successfully.")
-  )
-  .catch((err) => console.error("Unable to connect to the database:", err));
+  .then(() => {
+    console.log("Database connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+    process.exit(1); // Exit the process with an error code
+  });
 
 module.exports = sequelize;
